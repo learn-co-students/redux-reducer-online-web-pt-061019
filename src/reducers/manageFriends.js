7e13a99f0a9ba147cb0d3a5d43aece5f2b763851
Brittany Javalera <brittjavalera@gmail.com>
2020-03-31 13:02:32 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    let unfriend = state.friends.findIndex(friend => friend.id === action.id)
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            return {...state, friends: [...state.friends.slice(0, unfriend), 
+                ...state.friends.slice(unfriend + 1)]}
+        default:
+            return state
+    }
 }
