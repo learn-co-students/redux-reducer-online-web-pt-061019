4dda494013c4ad707666acdc7cf32ed39d9b4fe3
Khris Punzalan <khrispunzalan@gmail.com>
2020-03-30 18:20:51 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend] };
+    //   return { ...state, friends: state.friends.concat(action.friend)}
+    case 'REMOVE_FRIEND':
+      const newFriendSet = state.friends.filter((f) => f.id !== action.id);
+      return { ...state, friends: newFriendSet };
+    default:
+      return state;
+  }
 }
+
+// concat() method is used to join two or more strings. This method does not change the existing strings, but returns a new string containing the text of the joined strings.
