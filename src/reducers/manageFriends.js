32e09fa0a873d0efc419c7378ecd8f4bc8a186f1
maasa333 <konomaasa@gmail.com>
2020-04-07 19:56:16 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    state = {
+        friends: [],
+    }
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: state.friends.push(action.friend = {...friend, name, hometown, id})}
+        // case 'REMOVE_FRIEND':
+        //     return {}
+        default: 
+            return state;
+    }
 }
