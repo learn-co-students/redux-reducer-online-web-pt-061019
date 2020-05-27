7eb64f81ba3085391795cfaff8858b9e15b70881
maasa333 <konomaasa@gmail.com>
2020-05-26 22:00:19 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,8 +1,4 @@
-export function manageFriends(state, action){
-    // state = {
-    //     friends: [],
-    // }
-
+export function manageFriends(state={friends: []}, action){
     switch (action.type) {
         case 'ADD_FRIEND':
             return {...state, friends: [...state.friends, action.friend]}
